6b767712d82e8d993d5141bc135bbd903d26fa78
kimkangyun <rlarkddbs3@osstem.com>
2023-08-01 09:29:42 +0900

    Feign Client를 통한 서비스간 통신 및 ErrorDecoder설정

--- user-service/src/main/java/com/example/userservice/service/UserServiceImpl.java
+++ user-service/src/main/java/com/example/userservice/service/UserServiceImpl.java
@@ -1,19 +1,26 @@
 package com.example.userservice.service;
 
+import com.example.userservice.client.OrderServiceCleint;
 import com.example.userservice.dto.UserDto;
 import com.example.userservice.jpa.UserEntity;
 import com.example.userservice.jpa.UserRepository;
 import com.example.userservice.vo.ResponseOrder;
+import feign.FeignException;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.modelmapper.ModelMapper;
 import org.modelmapper.convention.MatchingStrategies;
 import org.modelmapper.spi.MatchingStrategy;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.core.env.Environment;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -22,13 +29,16 @@ import java.util.UUID;
 @Slf4j
 @Service
 @RequiredArgsConstructor
-public class UserServiceImpl implements UserService{
+public class UserServiceImpl implements UserService {
 
     private final UserRepository userRepository;
     private final BCryptPasswordEncoder bCryptPasswordEncoder;
+    private final RestTemplate restTemplate;
+    private final Environment environment;
+    private final OrderServiceCleint orderServiceCleint;
 
     @Override
-    public UserDto createUser(UserDto userDto){
+    public UserDto createUser(UserDto userDto) {
         userDto.setUserId(UUID.randomUUID().toString());
 
         ModelMapper mapper = new ModelMapper();
@@ -49,14 +59,31 @@ public class UserServiceImpl implements UserService{
     public UserDto getUserByUserId(String userId) {
         UserEntity userEntity = userRepository.findByUserId(userId);
 
-        if(userEntity == null) {
+        if (userEntity == null) {
             throw new UsernameNotFoundException("User not Found");
         }
 
         UserDto userDto = new ModelMapper().map(userEntity, UserDto.class);
 
-        List<ResponseOrder> orders = new ArrayList<>();
-        userDto.setOrders(orders);
+//        String orderUrl = String.format(environment.getProperty("order_service.url"),userId);
+//        ResponseEntity<List<ResponseOrder>> orderListResponse =
+//                restTemplate.exchange(orderUrl, HttpMethod.GET, null, new ParameterizedTypeReference<List<ResponseOrder>>() {
+//        });
+//        List<ResponseOrder> orderList = orderListResponse.getBody();
+
+
+        /* Using a feign Client*/
+//        List<ResponseOrder> orderList = null;
+//        try{
+//            orderList = orderServiceCleint.getOrders(userId);
+//        } catch(FeignException ex){
+//            log.error(ex.getMessage());
+//        }
+
+        List<ResponseOrder> orderList = orderServiceCleint.getOrders(userId);
+        userDto.setOrders(orderList);
+
+        userDto.setOrders(orderList);
 
         return userDto;
     }
@@ -70,12 +97,12 @@ public class UserServiceImpl implements UserService{
     public UserDto getUserDetailsByEmail(String email) {
         UserEntity userEntity = userRepository.findByEmail(email);
 
-        if(userEntity == null){
+        if (userEntity == null) {
             throw new UsernameNotFoundException(email);
         }
 
 
-        UserDto userDto = new ModelMapper().map(userEntity,UserDto.class);
+        UserDto userDto = new ModelMapper().map(userEntity, UserDto.class);
         return userDto;
     }
 
@@ -83,10 +110,10 @@ public class UserServiceImpl implements UserService{
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
         UserEntity userEntity = userRepository.findByEmail(username);
 
-        if(userEntity == null){
+        if (userEntity == null) {
             throw new UsernameNotFoundException(username);
         }
 
-        return new User(userEntity.getEmail(),userEntity.getEncryptedPwd(),true,true,true,true,new ArrayList<>());
+        return new User(userEntity.getEmail(), userEntity.getEncryptedPwd(), true, true, true, true, new ArrayList<>());
     }
 }
